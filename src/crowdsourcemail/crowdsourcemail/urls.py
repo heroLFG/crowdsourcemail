50c5c02c13ce12e90b0945efab194904c184b629
Gavin Palmer <gavin@herolfg.com>
2021-07-18 07:12:12 -0500

    wip: get token for anonymous user

--- src/crowdsourcemail/crowdsourcemail/urls.py
+++ src/crowdsourcemail/crowdsourcemail/urls.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from mail import views
 
 router = routers.DefaultRouter()
+router.register(r'messages/public', views.PublicMessageViewSet)
 router.register(r'messages', views.MessageViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api/', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('polls/', include('polls.urls')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include('drfpasswordless.urls')),
-    path('', include('frontend.urls')),  
+    re_path('^.*', include('frontend.urls')),  
 ]
--- src/crowdsourcemail/mail/views.py
+++ src/crowdsourcemail/mail/views.py
@@ -8,6 +8,14 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows messages to be viewed or edited.
     """
-    queryset = Message.objects.all() #.order_by('-date_joined')
+    queryset = Message.objects.all().order_by('-processed')
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class PublicMessageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows public messages to be viewed or edited.
+    """
+    queryset = Message.objects.all().order_by('-processed')
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
