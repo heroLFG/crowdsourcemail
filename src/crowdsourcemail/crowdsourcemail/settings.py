7c0f8bec66944531a46aa016d6559158fd37a564
Gavin Palmer <gavin@herolfg.com>
2021-07-19 07:31:34 -0500

    hide debug in prod

--- src/crowdsourcemail/crowdsourcemail/settings.py
+++ src/crowdsourcemail/crowdsourcemail/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('ENV') != 'env_test'
 
 ALLOWED_HOSTS = [os.getenv('HOST')]
 
