733979a5f53ce991c360e901fa7c8046afb06642
Gavin Palmer <gavin@herolfg.com>
2021-07-26 07:14:01 -0500

    table looks better

--- src/frontend/src/crowdsourcemail/crowdsourcemail.js
+++ src/frontend/src/crowdsourcemail/crowdsourcemail.js
@@ -20,20 +20,19 @@ const crowdsourcemail = {
         const table = $('<table class="table table-striped"></table>');
         const tbody = $('<tbody></tbody>');
         $.each(mailData.results, (index, message) => {
-            console.log('message', message);
             const messageListItem = crowdsourcemail.getMessageListItem(message);
             tbody.append(messageListItem);
         });
         table.append(tbody);
-        const container = $('<div class="container-fluid bg-light"></div>');
+        const container = $('<div class="bg-light"></div>');
         container.append(table);
         $('.app').html(container);
     },
 
     getMessageListItem: (message) => {
         const row = $('<tr></tr>');
-        const text = $(`<td class="col-11"><div class="message"><span class="subject">${message.subject}</span><span class="text">${message.text}</span></div></td>`);
-        const time = $(`<td class="col-1">${moment(message.processed).format("M/D/YY")}</td>`);
+        const text = $(`<td class="col-9 col-sm-10 col-lg-11"><div class="message"><span class="subject">${message.subject}</span><span class="text">${message.text}</span></div></td>`);
+        const time = $(`<td class="col-3 col-sm-2 col-lg-1">${moment(message.processed).format("M/D/YY")}</td>`);
         row.append(text);
         row.append(time);
         return row;
